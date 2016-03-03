cd344ba7362af344fa5f4f1cf0bb93887becb86c
Babosa <mysunpany@gmail.com>
2016-03-03 15:51:15 +0800

    Release EasyPublisher v1.0.16.0303

---
+++ Android/src/org/easydarwin/easypublisher/EasyPublisherJni.java
@@ -0,0 +1,39 @@
+package org.easydarwin.easypublisher;
+
+public class EasyPublisherJni {
+	
+	/**
+	 * Initialized publisher with width and height.
+	 *
+	 * <pre>This function must be called firstly.</pre>
+	 *
+	 * @return {0} if successful
+	 */
+    public native int EasyPublisherInit(int width, int height);
+	
+	/**
+	* start to publish stream.
+	*
+	* @param url
+	*
+	* @return {0} if successful
+	*/
+    public native int EasyPublisherStartPublish(String url);
+	
+	/**
+	* set live video data.
+	*
+	* @param cameraType: CAMERA_FACING_BACK with 0, CAMERA_FACING_FRONT with 1
+	* @param curOrg: LANDSCAPE with 0, PORTRAIT 1
+	*
+	* @return {0} if successful
+	*/
+    public native int EasyPublisherOnCaptureVideoData(byte[] data, int len, int cameraType, int curOrg);
+	
+	/**
+	 * Stop publisher.
+	 *
+	 * @return {0} if successful
+	 */
+    public native int EasyPublisherStopPublish();
+}
